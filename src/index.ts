9c2d597bd861489243d1ff570897105a1c2e8782
xcfio <119097812+xcfio@users.noreply.github.com>
2026-04-06 19:22:47 +0600

    feat: add set methods

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { DatabaseSync, DatabaseSyncOptions } from "node:sqlite"
 export { version } from "../package.json"
 
-export class SqliteMap<K, V> {
+export class SqliteMap<K extends string, V> {
     private db: DatabaseSync
 
     constructor(path: string, options?: DatabaseSyncOptions) {
         this.db = new DatabaseSync(path, options)
         this.db.exec("CREATE TABLE IF NOT EXISTS map (key TEXT PRIMARY KEY, value TEXT)")
+    }
+
+    set(key: K, data: V): this {
+        const stmt = this.db.prepare(`INSERT OR REPLACE INTO cache (key, value) VALUES (?, ?)`)
+        stmt.run(String(key), JSON.stringify(data), Date.now())
+        return this
     }
 }
